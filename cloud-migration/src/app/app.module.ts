4805df193eed9ba2117aabd4b7694801888093c2
kallah Ben Ahmeth <fallmoussa994@gmail.com>
2021-02-15 12:01:24 +0000

    updated

--- cloud-migration/src/app/app.module.ts
+++ cloud-migration/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { CriteriaComponent } from './criteria-management/criteria/criteria.component';
 
 import {MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenav, MatSidenavModule} from '@angular/material/sidenav';
@@ -16,14 +17,15 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { SidenavService } from './services/sidenav.service';
-
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [
     AppComponent,
     CriteriaManagementComponent,
     DashboardComponent,
     SidebarComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    CriteriaComponent
   ],
   imports: [
     BrowserModule,
@@ -35,6 +37,7 @@ import { SidenavService } from './services/sidenav.service';
     MatIconModule,
     MatButtonModule,
     MatListModule,
+    MatCardModule
   ],
   providers: [SidenavService, MatSidenav],
   bootstrap: [AppComponent]
