b4e8d2c9dbdf797158e15a682b2f4ac5499ba6a5
kallah Ben Ahmeth <fallmoussa994@gmail.com>
2021-03-16 14:03:14 +0000

    done rule-management

--- cloud-migration/src/app/rules-management/rules-management.component.ts
+++ cloud-migration/src/app/rules-management/rules-management.component.ts
@@ -20,21 +20,19 @@ export class RulesManagementComponent implements OnInit, AfterViewInit {
   dataSource = new MatTableDataSource<Rule>();
   displayedColumns = ['name', 'criticity', 'complexity', 'availability', 'type', 'action'];
   formGroup!: FormGroup;
+  selected = 'type';
   boolCreate = true;
   panelOpenState = false;
-  criterion0!: Criteria;
-  criterion1!: Criteria;
-  criterion2!: Criteria;
   status = false;
   pageTitle: string | undefined;
   formTitle: string | undefined;
+  types!: string[];
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
 
   constructor(
     private ruleService: RulesService,
-    private criteriaService: CriteriaStoreService,
     public dialog: MatDialog,
     private formBuilder: FormBuilder,
     private router: Router
@@ -49,7 +47,7 @@ export class RulesManagementComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
     this.getAllrules();
     this.newRuleForm();
-    this.getAllcriteriaValue();
+    this.types = ['Big Data', 'Web', 'Mobile'];
   }
 
   getAllrules(): void {
@@ -58,13 +56,6 @@ export class RulesManagementComponent implements OnInit, AfterViewInit {
       console.log(data);
     });
   }
-
-  getAllcriteriaValue(): void {
-    this.criteriaService.getCriteria().then( data => {
-      console.log(' Criteria ', data.criteres);
-    });
-  }
-
   applyFilter(event: Event): void {
     const filterValue = ( event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
@@ -84,9 +75,6 @@ export class RulesManagementComponent implements OnInit, AfterViewInit {
        type: ['', [Validators.required]]
      });
    }
-  delete(name: string): void {
-  }
-
 
   reset(form: FormGroup): void {
     form.reset();
@@ -111,6 +99,10 @@ export class RulesManagementComponent implements OnInit, AfterViewInit {
     if (this.boolCreate) {
       this.createRuleConfirmation(rule);
     }
+    else {
+      this.updateRuleConfirmation(rule);
+      console.log(rule);
+    }
   }
 
   createRuleConfirmation(rule: Rule): void {
@@ -133,10 +125,69 @@ export class RulesManagementComponent implements OnInit, AfterViewInit {
     });
   }
 
+  deleteRuleConfirmation(name: string): void {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '200px',
+      data: {
+        message: 'Confirm to drop this criterion ?'
+      },
+    });
+    dialogRef.afterClosed().subscribe( result => {
+      if (result) {
+        this.ruleService.deleteRule(name).then( data => {
+          console.log(data);
+        })
+        .catch( e => {
+          console.log(e);
+        });
+        this.router.navigate(['']);
+      }
+    });
+  }
+
+  updateRuleConfirmation(rule: Rule): void {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      width: '200px',
+      data: {
+        message: 'Confirm tu update this Rule!'
+      }
+    });
+    dialogRef.afterClosed().subscribe( result => {
+      if (result) {
+        this.ruleService.updateRule(rule).then( data => {
+          console.log(data);
+        })
+        .catch(e => {
+          console.log(e);
+        });
+        this.router.navigate(['']);
+      }
+    });
+  }
+
   edit(row: any): void {
+    this.panelOpenState = true;
+    this.formTitle = 'Update Rule ' + row.name;
+    this.f.name.setValue(row.name);
+    this.f.criticity.setValue(row.criticity);
+    this.f.complexity.setValue(row.complexity);
+    this.f.availability.setValue(row.availability);
+    this.f.type.setValue(row.type);
+    this.status = true;
+    this.boolCreate = false;
   }
 
   detail(row: any): void {
+  }
+
+  delete(name: string): void {
+    this.deleteRuleConfirmation(name);
+  }
+
+  back(): void {
+    this.newRuleForm();
+    this.boolCreate = true;
+    this.panelOpenState = false;
   }
 }
 
